02ab1befd6014d30ace046e4e3beccaf307b654a
maria <maria_c_brito@estudante.sesisenai.org.br>
2026-03-24 10:01:17 -0300

    add: primeiro exercicio de js

---
+++ ex_01.js
@@ -0,0 +1,15 @@
+function somarNumeros( ) {
+    let soma = 0;
+    let continuar = true; 
+
+    while(continuar) {
+        let numero = parseFloat(prompt("Digite um número (ou digite 0 para finalizar):"));
+        if(!isNaN(numero)) {
+            soma += numero;
+     } else {
+            alert("Por favor, digite um número válido!");
+        }
+        continuar = confirm("Deseja adicionar mais números?");
+    }
+    alert("A soma dos números é: " + soma);
+}
